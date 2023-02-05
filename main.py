5d2cb64ffde59cf97fe5bc729de249209bc4bed4
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-05 17:06:24 +0000

    read json and config.json

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
-
+import json
 
 
 driver = webdriver.Chrome()
+
+print(Colorate.Color(Colors.green, 'Reading json...', True))
+
+
+f = open('config.json', 'r')
+data = json.load(f)
