7ae75a49d183f619d732ba1381606c40452bdc7e
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-05 17:04:01 +0000

    imports

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from pystyle import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+
+
+driver = webdriver.Chrome()
